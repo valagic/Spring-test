cc1cdccbffaa5b63567d3d14f86137d6914a917f
Vedran Alagić <59015187+valagic@users.noreply.github.com>
2021-11-08 22:31:09 +0100

    kreiranje testnog projekta

---
+++ test/src/main/java/testaplikacija/test/AplikacijaTestApplication.java
@@ -0,0 +1,13 @@
+package testaplikacija.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikacijaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikacijaTestApplication.class, args);
+	}
+
+}
---
+++ test/src/test/java/testaplikacija/test/AplikacijaTestApplicationTests.java
@@ -0,0 +1,13 @@
+package testaplikacija.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AplikacijaTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
